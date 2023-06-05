94fdf2b07796bb5be239d20f8905f5ca5a5bfe65
Christoffer Winterkvist <christoffer@winterkvist.com>
2023-06-05 18:50:40 +0200

    Set platform version to macOS(v12)

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
   name: "Dock",
-  platforms: [.macOS(.v13)],
+  platforms: [.macOS(.v12)],
   products: [
     .library(name: "Dock", targets: ["Dock"]),
   ],
